f509d286f50c9e91a8e13780083c8289b98bd87e
jisung <jisung.smart@gmail.com>
2021-04-28 12:39:13 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+import smtplib
+
+app = Flask(__name__)
+
+OWN_EMAIL = "jisungstrong@gmail.com"
+OWN_PASSWORD = "wltjd6230"
+
+
+@app.route('/', methods=["GET", "POST"])
+def main_page():
+    if request.method == "POST":
+        data = request.form
+        print(f"{data['name']} {data['email']} {data['phone']} {data['message']}")
+        send_email(data['name'], data['email'], data['phone'], data['message'])
+        return render_template("index.html", msg_sent=True)
+    return render_template("index.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
